23a86a50e2725bbcbfd07d42b5861933809397b2
wertalp <patrick.wertal@gmail.com>
2017-03-10 19:58:10 +0100

    working on Viewer class

---
+++ at.wertal.db.test/src/at/wertal/db/test/Viewer.java
@@ -0,0 +1,14 @@
+package at.wertal.db.test;
+
+public class Viewer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public String getName() {
+		return "Test1";
+	}
+}
